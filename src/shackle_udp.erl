bbe850a5a1f4f8fd34332942c0324b1c174d8877
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-12-01 10:14:46 -0500

    Cleanup

--- src/shackle_udp.erl
+++ src/shackle_udp.erl
@@ -2,10 +2,23 @@
 -include("shackle_internal.hrl").
 
 -export([
+    close/1,
+    header/2,
     new/3,
-    send/3,
-    close/1
+    send/3
 ]).
+
+%% public
+-spec close(inet:socket()) -> ok.
+
+close(Socket) ->
+    gen_udp:close(Socket).
+
+-spec header(inet:ip_address(), inet:port_number()) -> iodata().
+
+header({A, B, C, D}, Port) ->
+    [[((Port) bsr 8) band 16#ff, (Port) band 16#ff],
+            [A band 16#ff, B band 16#ff, C band 16#ff, D band 16#ff]].
 
 -spec new(inet:ip_address(), inet:port_number(), [gen_udp:connect_option()]) ->
     {ok, inet:socket()} | {error, term()}.
@@ -16,17 +29,10 @@ new(_Ip, _Port, Options) ->
 -spec send(inet:socket(), iodata(), iodata()) -> ok | {error, term()}.
 
 send(Socket, Header, Data) ->
-    try erlang:port_command(Socket, [Header, Data]) of
-        true ->
-            ok;
-        false ->
-            {error, command_aborted}
+    try
+        true = erlang:port_command(Socket, [Header, Data]),
+        ok
     catch
         Error:Reason ->
             {error, {Error, Reason}}
     end.
-
--spec close(inet:socket()) -> ok.
-
-close(Socket) ->
-    gen_udp:close(Socket).
