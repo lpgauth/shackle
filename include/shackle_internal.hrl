01fc66a53fbfba3a974aa3541cdad719e0a45362
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-01-13 22:22:05 -0500

    Make round_robin pool strategy the default

--- include/shackle_internal.hrl
+++ include/shackle_internal.hrl
@@ -12,7 +12,7 @@
 -define(DEFAULT_BACKLOG_SIZE, 1024).
 -define(DEFAULT_IP, "127.0.0.1").
 -define(DEFAULT_POOL_SIZE, 16).
--define(DEFAULT_POOL_STRATEGY, random).
+-define(DEFAULT_POOL_STRATEGY, round_robin).
 -define(DEFAULT_PROTOCOL, shackle_tcp).
 -define(DEFAULT_RECONNECT, true).
 -define(DEFAULT_RECONNECT_MAX, timer:minutes(2)).
